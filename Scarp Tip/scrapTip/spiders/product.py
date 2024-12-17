ebbd706ab455fe7a08a6682002c0ae1d1c818439
Chy-Zaber-Bin-Zahid <czaber47@gmail.com>
2024-12-17 11:28:11 +0600

    done

--- Scarp Tip/scrapTip/spiders/product.py
+++ Scarp Tip/scrapTip/spiders/product.py
@@ -7,7 +7,7 @@ import requests
 
 
 class ProductSpider(scrapy.Spider):
-    name = "scrapTip"
+    name = "Scarp Tip"
     allowed_domains = ["uk.trip.com"]
     start_urls = ["https://uk.trip.com/hotels/?locale=en-GB&curr=GBP"]
     def parse(self, response):
